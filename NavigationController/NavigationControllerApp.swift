645c7def961c16a1ad95d54186a2e1dce7393af6
DariaKozlovska <odriy.lovska@gmail.com>
2025-03-27 21:12:58 +0100

    Initial Commit

---
+++ NavigationController/NavigationControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationControllerApp.swift
+//  NavigationController
+//
+//  Created by Daria Kozlovska on 27/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct NavigationControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
